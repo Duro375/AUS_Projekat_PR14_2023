87e4a67d97b5d8da2a3ed576783d35a09a799aa7
Duro375 <denisdjuris@gmail.com>
2026-04-22 15:58:57 +0200

    Implemented PackRequest() and ParseResponse() methods in ReadInputRegistersFunction.cs file

--- Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -24,15 +24,40 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters readParams = CommandParameters as ModbusReadCommandParameters;
+            byte[] ret_val = new byte[12];
+            ret_val[1] = (byte)(readParams.TransactionId);
+            ret_val[0] = (byte)(readParams.TransactionId >> 8);
+            ret_val[3] = (byte)(readParams.ProtocolId);
+            ret_val[2] = (byte)(readParams.ProtocolId >> 8);
+            ret_val[5] = (byte)(readParams.Length);
+            ret_val[4] = (byte)(readParams.Length >> 8);
+            ret_val[6] = readParams.UnitId;
+            ret_val[7] = readParams.FunctionCode;
+            ret_val[9] = (byte)(readParams.StartAddress);
+            ret_val[8] = (byte)(readParams.StartAddress >> 8);
+            ret_val[11] = (byte)(readParams.Quantity);
+            ret_val[10] = (byte)(readParams.Quantity >> 8);
+            return ret_val;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> ret_val = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int byteCnt = response[8];
+
+            ushort adress = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            for (int i = 0; i < byteCnt / 2; i += 2)
+            {
+                ushort value = (ushort)((ushort)(response[9 + i] << 8) + response[10 + i]);
+                Tuple<PointType, ushort> index = Tuple.Create(PointType.ANALOG_INPUT, (ushort)(adress + i));
+                ret_val.Add(index, value);
+            }
+
+            return ret_val;
         }
     }
 }
